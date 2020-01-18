76e037d7e5c6c854cb33e942410e384792dd3dae
anantpad <srid_24@hotmail.com>
2020-01-17 20:37:15 -0800

    attendance.html added

--- app.py
+++ app.py
@@ -2,17 +2,20 @@
 #import packages
 from flask import Flask, request, render_template
 from flask_pymongo import PyMongo
-from flask import redirect
-from datetime import datetime
+from flask import redirect, session
+from datetime import datetime, date
 
 #initialize
 #this function initializes any application
 app = Flask(__name__)
 app.debug = True
 
+app.secret_key = "sri"
+
 #config
 app.config["MONGO_URI"] = "mongodb://sridhar:asdf@cluster0-shard-00-00-aou9c.mongodb.net:27017,cluster0-shard-00-01-aou9c.mongodb.net:27017,cluster0-shard-00-02-aou9c.mongodb.net:27017/studentdb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 # app.config["MONGO_URI"] = "mongodb://sridhar:asdf@cluster0-aou9c.mongodb.net/test?retryWrites=true&w=majority"
+
 mongo = PyMongo(app)
 print(mongo.db)
 
@@ -22,10 +25,18 @@ def login():
     if request.method == 'GET':
         return render_template("login.html")
     else:
+
         studentid = request.form["studentid"]
+        session["loggedin"] = studentid
+        print(session)
         checkintime = datetime.now()
+        checkinday = checkintime.strftime("%a")
+        checkindate = checkintime.strftime("%d")
+        checkinmonth = checkintime.strftime("%b")
+        checkinyear = checkintime.strftime("%Y")
+
         mongo.db.attendance.update_one(
-            {"studentid":studentid},{"$set":{"checkintime":checkintime}},upsert=True
+            {"studentid":studentid},{"$set":{"checkindate":checkindate,"checkintime":checkintime,"checkinday":checkinday,"checkinmonth":checkinmonth,"checkinyear":checkinyear}},upsert=True
         )
         return redirect("/attendance")
 
@@ -52,21 +63,57 @@ def list():
         print(data)
         return render_template("studentList.html", data = data)
 
-# @app.route("/editStudent", methods = ['GET', 'POST'])
-# def edit():
-#     if request.method == 'POST':
-#
+@app.route("/editStudent", methods = ['GET', 'POST'])
+def editStudent():
+    if request.method == 'GET':
+        studentid = request.args['studentid']
+        record = mongo.db.user.find_one({"studentid":studentid})
+        firstname = record["firstname"]
+        # contents of this variable is the value of the input field
+        # GET, request.args gives argument fro URL
+        print(studentid)
+        return render_template('editUser.html',studentid=studentid, firstname=firstname)
+    # studentid=studentid :: name of variable on html template : variable studentid in the above function
+    else:
+        studentid = request.form["studentid"]
+        # REQUEST.FORM is from input field, name attribute
+        # contents of this variable is the value of the input field
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        mi = request.form["mi"]
+        gender = request.form["gender"]
+        birthdate = request.form["birthdate"]
+        mongo.db.user.update_one(
+            {"studentid": studentid},{"$set":{"firstname": firstname, "lastname": lastname, "mi": mi, "gender": gender,
+            "birthdate": birthdate}})
+        return redirect('/studentList')
 
-@app.route("/attendanceList", methods = ['GET', 'POST'])
+@app.route("/attendance", methods = ['GET', 'POST'])
 def attndlist():
     if request.method == 'GET':
+        if "loggedin" not in session:
+            return redirect("/")
+        elif session["loggedin"] != True:
+            return redirect("/")
+
         attenddata = mongo.db.attendance.find({})
         return render_template("attendance.html", attenddata = attenddata)
 
 @app.route("/delete", methods = ['GET', 'POST'])
 def delete():
-    if request.method == ['GET']:
-        return request.args
+    if request.method == 'GET':
+        studentid = request.args['studentid']
+        print(studentid)
+        mongo.db.user.delete_one({'studentid':studentid})
+        return redirect("/studentList")
+
+@app.route("/logout", methods = ['GET'])
+def logout():
+    session.pop("loggedin",None)
+    print(session)
+    return redirect("/")
+
+
 #run
 if __name__ == "__main__":
     app.run()
